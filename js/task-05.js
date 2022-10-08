5b9464f727ac9a89afa303aaeb4053343f1b4593
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-08 16:04:52 +0300

    Update task-05.js
    ---------

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,15 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
- 
+
 nameInput.addEventListener ('input', onInputInput);
 
 function onInputInput () {
-    nameOutput.textContent = nameInput.value;          
-}
+    nameOutput.textContent = nameInput.value; 
+    if (nameInput.value == '') {
+      nameOutput.textContent = 'Anonymous';
+    }
+}   
+
+ 
 
 
