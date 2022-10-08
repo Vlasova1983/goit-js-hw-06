fcc5d427956996f11823633fff15d8fbead33836
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-08 16:04:48 +0300

    Update task-02.js
    -------

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,22 @@ const ingredients = [
   'Condiments',
 ];
 
-const list = document.querySelector("#ingredients");
+// const list = document.querySelector("#ingredients");
 
+// for (let i=0; i<=ingredients.length-1;i+=1){
+//  const lastItem = document.createElement("li");
+//  lastItem.classList.add('item')
+//  lastItem.textContent = ingredients[i];
+//  list.append(lastItem);
+// }
+
+const classLi ='item';
+const listIngredients =[];
 for (let i=0; i<=ingredients.length-1;i+=1){
- const lastItem = document.createElement("li");
- lastItem.classList.add('item')
- lastItem.textContent = ingredients[i];
- list.append(lastItem);
+  const ingredient = ingredients[i];
+  const listIngredient= `<li class= "${classLi}"> ${ingredient} </li>` 
+  listIngredients.push(listIngredient);
 }
+const listEl = document.querySelector("#ingredients");
+listEl.insertAdjacentHTML('beforeend', listIngredients.join(''));
+  
