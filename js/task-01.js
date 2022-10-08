eca9e4d767c6bc4ece5885f00bfd1e6c2d82de1d
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-08 16:04:44 +0300

    Update task-01.js
    ----

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,20 @@
-const itemCategories = document.querySelectorAll('.item');
-console.log ('Number of categories:', itemCategories.length);
-const titelCategories = document.querySelectorAll('h2');
+// const itemCategories = document.querySelectorAll('.item');
+// console.log ('Number of categories:', itemCategories.length);
+
+
+// const titelCategories = document.querySelectorAll('h2');
+// const elementCategories = document.querySelectorAll('#categories ul');
+
+// for (let i=0; i<=titelCategories.length-1; i+=1) {
+//    console.log ('Category:', titelCategories[i].textContent);
+//    console.log ('Elements:',elementCategories[i].children.length); 
+// }
+
+
 const elementCategories = document.querySelectorAll('#categories ul');
-
-for (let i=0; i<=titelCategories.length-1; i+=1) {
-   console.log ('Category:', titelCategories[i].textContent);
-   console.log ('Elements:',elementCategories[i].children.length); 
+console.log ('Number of categories:',elementCategories.length );
+const itemCategories = document.querySelectorAll('.item');
+for (let i=0; i<=elementCategories.length-1; i+=1) {
+   console.log('Category:',itemCategories[i].firstElementChild.textContent);
+   console.log('Elements:', itemCategories[i].lastElementChild. childElementCount);
 }
-
-
-
-
-
-
-
